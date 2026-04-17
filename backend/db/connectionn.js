f54391a755b9adfb38e9fac0abc8469016e69a01
josefo0330 <jose_6128@hotmail.com>
2026-04-17 18:18:31 -0500

    db conf

--- backend/db/connectionn.js
+++ backend/db/connectionn.js
@@ -1,8 +1,8 @@
 const mysql = require("mysql")
 const db = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
+  host: process.env.DB_HOST || "maria_db_app",
   user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
+  password: process.env.DB_PASSWORD || "qwert",
   database: process.env.DB_DATABASE || "tareas"
 })
 module.exports = db
