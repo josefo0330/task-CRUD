5b030b36270da9183df321a95fb32cabbe8056b1
josefo0330 <jose_6128@hotmail.com>
2026-04-18 17:36:13 -0500

    basde datos conex

--- backend/db/connectionn.js
+++ backend/db/connectionn.js
@@ -1,8 +1,9 @@
-const maraiadb = require("mariadb")
+const maraiadb = require("mysql")
 const db = maraiadb.createPool({
-  host:  "maria_db_app",
+  host:  "bk88grkwbdul5pr44zeo-mysql.services.clever-cloud.com",
   user:  "root",
-  password:  "qwert",
-  database: "tareas"
+  password:  "ePcdXKC6brupmy7d62u34ylxcqdncUB6pRKG",
+  database: "bk88grkwbdul5pr44zeo",
+  port: 3306
 })
 module.exports = db
