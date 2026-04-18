4a6b13a48ad89a25c338e9306010a774f5c67dce
josefo0330 <jose_6128@hotmail.com>
2026-04-17 20:43:18 -0500

    compose update

--- backend/db/connectionn.js
+++ backend/db/connectionn.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql")
 const db = mysql.createPool({
-  host: process.env.DB_HOST || "maria_db_app",
+  host: process.env.DB_HOST || "tareas-db",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "qwert",
   database: process.env.DB_DATABASE || "tareas"
