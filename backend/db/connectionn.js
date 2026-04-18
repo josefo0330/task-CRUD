30da4fe3e629cca68d800327b78f979b22892248
josefo0330 <jose_6128@hotmail.com>
2026-04-18 08:43:01 -0500

    db conex change

--- backend/db/connectionn.js
+++ backend/db/connectionn.js
@@ -1,6 +1,6 @@
 const maraiadb = require("mariadb")
 const db = maraiadb.createPool({
-  host: process.env.DB_HOST || "phpmyadmin.softapp.ch",
+  host: process.env.DB_HOST || "maria_db_app",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "qwert",
   database: process.env.DB_DATABASE || "tareas"
