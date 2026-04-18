0f435c084bb5e1e4ca74c5cd5df14c6f7ad16238
josefo0330 <jose_6128@hotmail.com>
2026-04-17 21:12:27 -0500

    docker compose

--- backend/db/connectionn.js
+++ backend/db/connectionn.js
@@ -1,6 +1,6 @@
 const maraiadb = require("mariadb")
 const db = maraiadb.createPool({
-  host: process.env.DB_HOST || "tareas-db",
+  host: process.env.DB_HOST || "mariadb_db",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "qwert",
   database: process.env.DB_DATABASE || "tareas"
