7781cd1d07ab022d5dab0745b63e9c163ba40821
josefo0330 <jose_6128@hotmail.com>
2026-04-18 15:48:28 -0500

    api change

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const authRoutes = require("./routes/authRoutes")
 //rutas
 app.use(cors())
 app.use(express.json())
-app.use("/auth", authRoutes)
+app.use("/api/auth", authRoutes)
 app.get("/api", (req, res) => {
   res.send("API funcionando 🚀")
 })
@@ -20,7 +20,7 @@ app.get('/tasks/:userId', (req, res)=>{
   })
 })
 // insertar los datos nuevos
-app.post('/create',(req,res) =>{
+app.post('/api/create',(req,res) =>{
     const { title, descripcion, userID } = req.body
     const sql = `
     INSERT INTO task (title, descripcion, estado, userID)
@@ -34,7 +34,7 @@ app.post('/create',(req,res) =>{
     })
 })
 //eliminar las tareas 
-app.delete('/task/:id',(req, res) =>{
+app.delete('/api/task/:id',(req, res) =>{
     const sql = "DELETE FROM task where id = ? "
     const id = req.params.id
     db.query(sql,[id],(err,data)=>{
@@ -43,7 +43,7 @@ app.delete('/task/:id',(req, res) =>{
     })
 })
 //cambiar el estado de la tarea
-app.post("/status/:id", (req, res) => {
+app.post('/api/status/:id', (req, res) => {
 
     const id = req.params.id
     const { estado } = req.body
