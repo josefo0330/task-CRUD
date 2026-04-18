bf3998933290c77b3b372c8b37dd4faf1dbc5ae4
josefo0330 <jose_6128@hotmail.com>
2026-04-17 22:12:35 -0500

    docker update

--- backend/db/connectionn.js
+++ backend/db/connectionn.js
@@ -1,6 +1,6 @@
 const maraiadb = require("mariadb")
 const db = maraiadb.createPool({
-  host: process.env.DB_HOST || "mariadb_db_app",
+  host: process.env.DB_HOST || "phpmyadmin.softapp.ch",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "qwert",
   database: process.env.DB_DATABASE || "tareas"
