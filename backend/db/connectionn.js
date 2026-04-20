7665c86a99bd180ab7bd00cb9044483029be4b6a
josefo0330 <jose_6128@hotmail.com>
2026-04-19 21:06:51 -0500

    DOCKER COMP AND DB

--- backend/db/connectionn.js
+++ backend/db/connectionn.js
@@ -1,9 +1,9 @@
 const maraiadb = require("mysql2")
 const db = maraiadb.createPool({
-  host: process.env.DB_HOST || "bk88grkwbdul5pr44zeo-mysql.services.clever-cloud.com",
-  database: process.env.DB_NAME || "bk88grkwbdul5pr44zeo",
-  user: process.env.DB_USER || "upmy7d62u34ylxcq",
-  password: process.env.DB_PASSWORD || "ePcdXKC6brdncUB6pRKG",
+  host: process.env.DB_HOST || "maria_db_app",
+  database: process.env.DB_NAME || "tareas",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "qwert",
   
 
 })
